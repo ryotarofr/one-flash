5b8d404dfdbd77d819a524e69d9b2300919fb156
ZIN718 <jin007180@gmail.com>
2023-12-26 00:53:13 +0900

    おはようございます

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Hello Next.js</div>
+
+      <h1>ふじい</h1>
     </main>
   )
 }
