8540867920345a867a0f168a71ba0f7857b7cdaf
ryotarofr <ryoryo.fr0608@icloud.com>
2023-12-29 18:25:03 +0900

    input preview

---
+++ app/(landing)/personal/page.tsx
@@ -0,0 +1,13 @@
+import { InputForm, InputWithButton } from "@/components/input-form"
+
+const Personal = () => {
+  return (
+    <div>
+      <div className="flex justify-center my-5">
+        <InputWithButton />
+      </div>
+    </div>
+  )
+}
+
+export default Personal
