c73d64e29c524e199aabf323631238a8c3b355b7
ryotarofr <ryoryo.fr0608@icloud.com>
2024-01-07 16:27:09 +0900

    add

--- components/model-carousel-item.tsx
+++ components/model-carousel-item.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export const ModelCarouselItem = ({ data }: { data: Model }) => {
   return (
 
-    <div className=' overflow-hidden flex'>
+    <div className='overflow-hidden flex'>
       {data.pictures.map((picture, index) => (
         <div key={index} className="flex-[0_0_100%]  aspect-video w-full  mx-4 overflow-hidden">
           <Image src={picture} alt="sample" width={2000} height={1500} className="w-full h-full object-cover rounded-lg" />
