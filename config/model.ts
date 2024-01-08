621dfed8de15614749a8bdf80fbaf8239d6f14a6
ZIN718 <jin007180@gmail.com>
2024-01-08 21:43:06 +0900

    モデルが追加できるか確認

--- config/model.ts
+++ config/model.ts
@@ -17,5 +17,29 @@ export const models = [
     name: "ちえり",
     pictures: ["/image/models/tieri-1.jpeg","/image/models/tieri-3.jpeg"],
   },
+  {
+    id: 4,
+    eria: "okayama",
+    name: "りあ",
+    pictures: ["/image/models/ria-1.jpg"],
+  },
+  {
+    id: 5,
+    eria: "okayama",
+    name: "ゆきち",
+    pictures: ["/image/models/yukichi-1.jpeg"],
+  },
+  {
+    id: 6,
+    eria: "okayama",
+    name: "みあ",
+    pictures: ["/image/models/mia-1.jpeg"],
+  },
+  {
+    id: 7,
+    eria: "okayama",
+    name: "kanako",
+    pictures: ["/image/models/kanako-1.jpeg","/image/models/kanako-2.jpeg","/image/models/kanako-3.jpeg","/image/models/kanako-4.jpeg","/image/models/kanako-5.jpeg"],
+  },
 ]
 // one-flash/public/image/models/tieri-1.jpeg
